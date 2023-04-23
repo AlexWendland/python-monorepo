38ba0634fab5dd76c92bd8cb19ae3dda807ef197
Alex Wendland <a.p.wendland@gmail.com>
2023-04-23 09:50:42 +0100

    Hook

---
+++ something.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    this_is_a_very_very_very_very_very_very_very_very_very_very_very_long_variable = (
+        1 + 1 + 1 + 1
+    )
+    print(
+        this_is_a_very_very_very_very_very_very_very_very_very_very_very_long_variable
+    )
